2a245c940721a110413303d0bc410e42557cf994
Gr3Ko-X <dev.hautmonde@hautmondeindia.com>
2025-07-24 11:09:01 +0530

    Refactor marketing page layout and enhance features section with additional icons. Adjusted styling for feature components to improve visual consistency.

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -10,7 +10,7 @@ import { Hero } from "@/components/hero";
 import { PricingGrid } from "@/components/pricing-grid";
 import { Subheading } from "@/components/subheading";
 import { Tools } from "@/components/tools";
-import { IconReceiptFilled } from "@tabler/icons-react";
+import { Icon3dCubeSphere, IconMoodConfuzedFilled, IconReceiptFilled } from "@tabler/icons-react";
 import { FeaturesSectionDemo } from "@/components/featuress";
 import { LogosWithBlurFlip } from "@/components/brandcloud";
 
@@ -23,14 +23,7 @@ export default function Home() {
       <Tools />
       
       <div className="pb-10 sm:pb-40 pt-10 sm:pt-20">
-      <FeatureIconContainer className="flex justify-center items-center overflow-hidden">
-          <IconReceiptFilled className="h-6 w-6 text-cyan-500" />
-          </FeatureIconContainer>
-          <Heading className="pt-4" id="security">Uncompromising Security</Heading>
-        <Subheading>
-        Your financial security is our top priority. TAPxCard implements multiple layers of protection to keep your assets safe.
-        </Subheading>
-        <FeaturesSectionDemo />
+      
         <FeatureIconContainer className="flex justify-center items-center overflow-hidden">
           <IconReceiptFilled className="h-6 w-6 text-cyan-500" />
         </FeatureIconContainer>
@@ -40,6 +33,19 @@ export default function Home() {
         </Subheading>
         <PricingGrid />
         
+
+        <div className="mt-20">
+          <FeatureIconContainer className="flex justify-center items-center overflow-hidden">
+            <Icon3dCubeSphere className="h-6 w-6 text-cyan-500" />
+          </FeatureIconContainer>
+          <Heading className="pt-4" id="security">
+            Uncompromising Security
+          </Heading>
+          <Subheading>
+            Your financial security is our top priority. TAPxCard implements multiple layers of protection to keep your assets safe.
+          </Subheading>
+          <FeaturesSectionDemo />
+        </div>
       </div>
       <FAQs />
       <LogosWithBlurFlip />
